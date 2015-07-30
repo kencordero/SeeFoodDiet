85c06b4493a2511f05bf44258cb31531a604a6dd
Kenneth Cordero <kencordero@gmail.com>
2015-07-29 22:08:36 -0500

    Added TableViewController as rootViewController

--- SeeFoodDiet/AppDelegate.swift
+++ SeeFoodDiet/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        let nc = UINavigationController(rootViewController: TableViewController())
+        self.window?.rootViewController = nc
+        
         return true
     }
 
